0b4203cb38f091428f8179b47adbb6a876eb4d64
tomasborek <tomasborek3@gmail.com>
2021-11-19 17:20:32 +0100

    Byl přidán title registraci a loginu

--- pages/login.jsx
+++ pages/login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
     return (
         <>
         <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            <title>Přihlásit se | Roomie</title>
         </Head>
         <div className="Login">
             <Link href="/"><i className="step-back fas fa-chevron-left"></i></Link>
--- pages/register.jsx
+++ pages/register.jsx
@@ -15,7 +15,7 @@ const Register = () => {
     return (
         <>
         <Head>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            <title>Registrace | Roomie</title>
         </Head>
         <div className="Register">
             <div className="blue-overlay"></div>
@@ -23,8 +23,6 @@ const Register = () => {
             <div className="register-content">
                     <RegisterBox />
             </div>
-                
-            
         </div>
         </>
     )
