f0b9ab1f7c62558c5d6758f2c3ee0d4dafbcf43b
tomasborek <tomasborek3@gmail.com>
2021-12-20 11:22:03 +0100

    Opraven překlep v kodu

--- components/Listing/Listing.jsx
+++ components/Listing/Listing.jsx
@@ -341,7 +341,7 @@ const Listing = ({type}) => {
                                         </div>
                                         {listingInfo ?
                                             <>
-                                            {adddedPfp ?
+                                            {addedPfp ?
                                                 <img src={URL.createObjectURL(addedPfp)}/>
                                                 :
                                                 <>
