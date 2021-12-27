3e5dff4462d1f9530507616685467185713f69ba
tomasborek <tomasborek3@gmail.com>
2021-12-27 07:39:06 +0100

    Byl přidán useRef pro předvolbu

--- contexts/RegisterContext.js
+++ contexts/RegisterContext.js
@@ -11,6 +11,7 @@ export  function RegisterProvider(props) {
     const [username, setUsername] = useState();
     const emailRef = useRef();
     const phoneRef = useRef();
+    const phoneCodeRef = useRef();
     const passwordRef = useRef();
     const passwordCheckRef = useRef();
     const monthRef = useRef();
@@ -20,6 +21,7 @@ export  function RegisterProvider(props) {
         usernameState: {username, setUsername},
         emailRef: emailRef,
         phoneRef: phoneRef,
+        phoneCodeRef: phoneCodeRef,
         passwordRef: passwordRef,
         passwordCheckRef: passwordCheckRef,
         dayRef: dayRef,
