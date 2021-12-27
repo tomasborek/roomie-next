53820d0e15cd9a454f30e354f57947342642669f
tomasborek <tomasborek3@gmail.com>
2021-12-27 10:55:41 +0100

    Byl vymazán nepoužívaný kod

--- contexts/StorageContext.js
+++ contexts/StorageContext.js
@@ -35,16 +35,11 @@ export const StorageProvider = ({children}) => {
     }
 
     const uploadListingImgs = (files, uid, existingImgs) => {
-        return new Promise((resolve, reject) => {
             // Describes the format of an uploaded image (jpg or png)
             let format;
             let storageRefs = [];
             // In the passed array, there might be undefined fields, we need to filter it out of that and store it in valid files
             let validFiles = [];
-            /* If we upload a image to storage, and we find out that there's already 
-            a image with the same index, but in different format, we need to delete the
-             first one first, so we store it's ref in this variable for later use*/
-            let filesForDelete = [];
             // We set cache control for the images so the updating is quick
             const metadata = {
                 cacheControl: 'public,max-age=0',
@@ -58,36 +53,17 @@ export const StorageProvider = ({children}) => {
                         const storageRef = ref(storage, `/users/${uid}/listingImgs/${index}${format}`);
                         storageRefs.push(storageRef);
                         validFiles.push(file);
-                        // If there is a existing image on the same index, we need to check it's format in order to decide if we need to delete it
-                        if(existingImgs && existingImgs[index]){
-                            // We get it's format
-                            let existingFormat = existingImgs[index].split(".");
-                            if(existingFormat[existingFormat.length - 1] != format){
-                                
-                                const forDeleteRef = ref(storage, `users/${uid}/listingImgs/${index}.${existingFormat[existingFormat.length - 1]}`);
-                                filesForDelete.push(forDeleteRef);
-                            }
-                        }
                     }
                 }
             })
 
-            uploadImgsToStorage(storageRefs, validFiles, metadata).then((response) => {
-                resolve(response);
-            }).catch((error) => {
-                reject(error);
-            })
-        })
+            return Promise.all(
+                validFiles.map((file,index) => {
+                    return uploadBytes(storageRefs[index], validFiles[index], metadata);
+                })
+            )
     }
 
-
-    const uploadImgsToStorage = (storageRefs, validFiles, metadata) => {
-        return Promise.all(
-            validFiles.map((file,index) => {
-                return uploadBytes(storageRefs[index], validFiles[index], metadata);
-            })
-        )
-    }
     const value = {
         uploadImg
     }
