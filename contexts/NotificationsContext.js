675ded95525deac426ab86c99c03a6dd0f28dd68
tomasborek <tomasborek3@gmail.com>
2021-12-29 16:54:24 +0100

    Fetchování notifikací probíhá v kontexti

--- contexts/NotificationsContext.js
+++ contexts/NotificationsContext.js
@@ -1,11 +1,36 @@
-import React,{useState, createContext, useContext} from 'react'
+import React,{useState, createContext, useContext, useEffect} from 'react'
+import {db} from "../Firebase";
+import { onSnapshot, collection } from 'firebase/firestore';
+//COntexts
+import { useAuth } from './AuthContext';
+import { useDb } from './DbContext';
+
+
+
 const NotificationsContext = createContext();
 export const useNotifications = () => {
     return useContext(NotificationsContext);
 }
 
 export const NotificationsProvider = ({children}) => {
+    const {currentUser} = useAuth();
+    const {getNotifications} = useDb();
     const [notifications, setNotifications] = useState(null);
+    useEffect(() => {
+        if(currentUser){
+            let newNotifications = [];
+           getNotifications(currentUser.uid)
+           .then((docs) => {
+                docs.docs.forEach(doc => {
+                    newNotifications.push(doc.data());
+                })
+                setNotifications(newNotifications);
+           })
+        }else{
+            setNotifications(null);
+        }
+    }, [currentUser])
+  
     return (
         <NotificationsContext.Provider value={[notifications, setNotifications]}>
             {children}
