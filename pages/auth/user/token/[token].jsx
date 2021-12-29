3ff6d23183495052c0c51a9e4d2371f7f8c75ecd
tomasborek <tomasborek3@gmail.com>
2021-12-29 07:58:59 +0100

    Byla přidána page (odkaz) na ovření emailu

---
+++ pages/auth/user/token/[token].jsx
@@ -0,0 +1,38 @@
+import React,{useState, useEffect} from 'react'
+//Next
+import { useRouter } from 'next/router'
+//Contexts
+import { useFunctions } from '../../../../contexts/FunctionsContext';
+//Components
+//MUI
+import { CircularProgress } from '@mui/material';
+
+const VerifyEmail = () => {
+    const {callable} = useFunctions();
+    const verifyToken = callable("emailVerification-verifyToken");
+    const router = useRouter();
+    const {token} = router.query;
+    // State
+    const [isVerified, setIsVerified] = useState(false);
+    const [failed, setFailed] = useState(false);
+
+    useEffect(() => {
+        if(router.isReady){
+
+            verifyToken(JSON.stringify({token: token})).then((response) => {
+                setIsVerified(true);
+            }).catch((error) => {
+                setFailed(true);
+            })
+        }
+    }, [router.isReady])
+    return (
+        <div className='VerifyEmail'>
+            <img src="/img/logos/logo-small.png"/>
+            <h2>Roomie.cz</h2>
+            {(!isVerified && !failed) ? <> <h3>Prosím vyčkete...</h3> <CircularProgress/> </> : isVerified ? <h3>Váš e-mail byl ověřen</h3> : failed ? <h3>Něco se nepodařilo.</h3> : ""}
+        </div>
+    )
+}
+
+export default VerifyEmail
