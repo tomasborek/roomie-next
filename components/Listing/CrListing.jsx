c0eb8f87036ca490effad1b86cf5c5fde4900fb9
tomasborek <tomasborek3@gmail.com>
2021-12-29 09:20:12 +0100

    Bux Fix: Překlep v názvu dialogu import

--- components/Listing/CrListing.jsx
+++ components/Listing/CrListing.jsx
@@ -33,7 +33,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import CustomDialog from '../CustomDiaog/CustomDialog';
+import CustomDialog from '../CustomDialog/CustomDialog';
 
 const CrListing = ({type}) => {
     const router = useRouter();
