399bdf21b2ebf1cc4b81ac06f2e181cf10ca4e79
tomasborek <tomasborek3@gmail.com>
2021-12-30 07:09:45 +0100

    Byla přidána cloud funkce pro kontrolu narozenin a zvyšování věku

---
+++ functions/scheduled.js
@@ -0,0 +1,35 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const db = admin.firestore();
+
+exports.birthday = functions.pubsub.schedule("0 1 * * *")
+    .timeZone("Europe/Prague")
+    .onRun((context) => {
+        const now = admin.firestore.Timestamp.now();
+        db.collection("users").where("birthday", "<=", now).get().then((users) => {
+            return Promise.all(
+                users.docs.map((user) => {
+                    const currentAge = user.data().mainInfo.age;
+                    const age = currentAge + 1;
+                    const listingId = user.data().listing.id;
+                    return new Promise((resolve, reject) => {
+                        db.collection("users").doc(user.id).update({
+                            "mainInfo.age": age,
+                        }).then((response) => {
+                            return db.collection("listings").doc(listingId).update({
+                                "userInfo.age": age,
+                            })
+                        }).then((response) => {
+                            resolve(response);
+                        }).catch((error) => {
+                            reject(error);
+                        })
+                    })
+                })
+            )
+        }).then((response) => {
+            return response;
+        }).catch((error) => {
+            return error;
+        })
+    })
