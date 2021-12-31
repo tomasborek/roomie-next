a9f2a7e3063adba5e785e2ca0dde67c1001ceebe
tomasborek <tomasborek3@gmail.com>
2021-12-31 05:56:49 +0100

    Input dialog byl udělán reusable

--- components/InputDialog/InputDialog.jsx
+++ components/InputDialog/InputDialog.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const InputDialog = ({setMessage, message, setOpen, handleSend}) => {
+const InputDialog = ({heading, description, setMessage, message, setOpen, handleSend}) => {
     return (
         <div className="req-dialog">
             <i onClick={() => setOpen(false)} className="dialog-close fas fa-times"></i>
-            <div className="dialog-heading">Odelsat žádost o kontakt</div>
-            <div className="dialog-description">Inzerant uvidí vaší žádost s vaší zprávou a může se rozhodnout zda vaší žádost přijme a poskytne vám své kontaktní údaje.</div>
+            <div className="dialog-heading">{heading}</div>
+            <div className="dialog-description">{description}</div>
             <textarea onChange={(e) => setMessage(e.target.value)} value={message} className="dialog-input" placeholder="Sem zadejte svou zprávu..."></textarea>
             <button onClick={handleSend} className="main-btn">Poslat <i className="fas fa-envelope"></i></button>
             
