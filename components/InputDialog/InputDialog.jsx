7ecff28186db5416d13d1cf776c53c3361541b5b
tomasborek <tomasborek3@gmail.com>
2021-12-31 06:00:31 +0100

    Byly omezeny breaks a počet charakterů

--- components/InputDialog/InputDialog.jsx
+++ components/InputDialog/InputDialog.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
 const InputDialog = ({heading, description, setMessage, message, setOpen, handleSend}) => {
+    const textAreaChange = (e) => {
+        let text = e.target.value; 
+        while (text.includes("\n\n\n")){
+            text = text.replace("\n\n\n","\n\n");
+        }
+       setMessage(text);
+    }
     return (
         <div className="req-dialog">
             <i onClick={() => setOpen(false)} className="dialog-close fas fa-times"></i>
             <div className="dialog-heading">{heading}</div>
             <div className="dialog-description">{description}</div>
-            <textarea onChange={(e) => setMessage(e.target.value)} value={message} className="dialog-input" placeholder="Sem zadejte svou zprávu..."></textarea>
+            <textarea maxLength={250} onChange={(e) => textAreaChange(e)} value={message} className="dialog-input" placeholder="Sem zadejte svou zprávu..."></textarea>
             <button onClick={handleSend} className="main-btn">Poslat <i className="fas fa-envelope"></i></button>
             
         </div>
