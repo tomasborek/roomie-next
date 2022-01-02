c13b8eafd9c89f7710478ef0e89a00eac48ac4be
tomasborek <tomasborek3@gmail.com>
2022-01-02 06:55:53 +0100

    Nové namespaces podle nových statů

--- components/Listing/ListingHeader/ListingHeader.jsx
+++ components/Listing/ListingHeader/ListingHeader.jsx
@@ -12,7 +12,7 @@ const ListingHeader = ({type, listingInfo}) => {
             <div className="content-header">
                 <div className="mid-container">
                     <div className="header-pfp-container">
-                        {listingInfo ? <img src={listingInfo.data().userInfo.gender === "male" ? "/img/pfps/radek-pfp.png" : "/img/pfps/radka-pfp.png"} className="header-pfp"></img> : <div className="header-pfp"></div> }   
+                        {listingInfo ? <img src={listingInfo.userInfo.gender === "male" ? "/img/pfps/radek-pfp.png" : "/img/pfps/radka-pfp.png"} className="header-pfp"></img> : <div className="header-pfp"></div> }   
                     </div>
 
                     {!listingInfo ? 
@@ -22,11 +22,11 @@ const ListingHeader = ({type, listingInfo}) => {
                         :   
                         <div className="header-info">
                             <div className="info-main">
-                                            <h1 className="main-name">{listingInfo.data().userInfo.name}</h1>
-                                            {currentUser && currentUser.uid == listingInfo.data().userInfo.uid &&<button onClick={() => setEditListing(prevState => !prevState)}className="main-edit-profile">{editListing ? "Zpět" : "Upravit profil"}</button>}
+                                            <h1 className="main-name">{listingInfo.userInfo.name}</h1>
+                                            {currentUser && currentUser.uid == listingInfo.userInfo.uid &&<button onClick={() => setEditListing(prevState => !prevState)}className="main-edit-profile">{editListing ? "Zpět" : "Upravit profil"}</button>}
                                             <i className="main-more fas fa-ellipsis-h"></i>
                                             <div className="main-description">
-                                                <p>{listingInfo.data().userInfo.age}, {listingInfo.data().userInfo.gender === "male" ? "muž" : listingInfo.data().userInfo.gender === "female" ? "žena" : "jiné"}</p>
+                                                <p>{listingInfo.userInfo.age}, {listingInfo.userInfo.gender === "male" ? "muž" : listingInfo.userInfo.gender === "female" ? "žena" : "jiné"}</p>
                                             </div>
                             </div>
                         <ListingInfoImportant type="flatmate"/>
@@ -52,11 +52,11 @@ const ListingHeader = ({type, listingInfo}) => {
                                 :
                                 <div className="header-info">
                                     <div className="info-main">
-                                        <h1 className="main-name">Byt {listingInfo && listingInfo.data().flatBoxes.layout && listingInfo.data().flatBoxes.layout}</h1>
-                                      {currentUser && listingInfo.data().userInfo.uid == currentUser.uid ? <button onClick={() => setEditListing(prevState => !prevState)} className="main-edit-profile">{editListing ? "Zpět" : "Upravit inzerát"}</button> : "" }  
+                                        <h1 className="main-name">Byt {listingInfo && listingInfo.flatBoxes.layout && listingInfo.flatBoxes.layout}</h1>
+                                      {currentUser && listingInfo.userInfo.uid == currentUser.uid ? <button onClick={() => setEditListing(prevState => !prevState)} className="main-edit-profile">{editListing ? "Zpět" : "Upravit inzerát"}</button> : "" }  
                                         <i className="main-more fas fa-ellipsis-h"></i>
                                         <div className="main-description">
-                                            <p>{listingInfo && listingInfo.data().flatBoxes.location}</p>
+                                            <p>{listingInfo && listingInfo.flatBoxes.location}</p>
                                         </div>
                                     </div>
                                     <ListingInfoImportant type="flat"/>
