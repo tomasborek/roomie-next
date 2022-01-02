bd8267d8d22cbd902b768f4fe05a41f96c6b6e40
tomasborek <tomasborek3@gmail.com>
2022-01-02 06:54:46 +0100

    Informace o listingu serverside

--- pages/flat/[id].jsx
+++ pages/flat/[id].jsx
@@ -1,17 +1,70 @@
 import React from 'react'
 //COmponents
 import Listing from '../../components/Listing/Listing'
+//Firebase
+import { db } from '../../Firebase';
+import { getDoc, doc } from 'firebase/firestore';
 
-const FlatListing = () => {
+const FlatListing = (props) => {
     return (
-        <Listing type="flat"/>
+        <Listing type="flat"
+            listingName={props.listingName}
+            listingFlatBio={props.listingFlatBio}
+            listingpersonBio={props.listingPersonBio}
+            listingPersonBoxes={JSON.parse(props.listingPersonBoxes)}
+            listingFlatBoxes={JSON.parse(props.listingFlatBoxes)}
+            listingPersonTags={JSON.parse(props.listingPersonTags)}
+            statusCode={props.statusCode}
+        />
     )
 }
 
 export default FlatListing
 
-
-
-
+export async function getServerSideProps(context) {
+    const {id} = context.params;
+    try{    
+        const docRef = await getDoc(doc(db, "listings", id));
+        const listingLayout = docRef.data().flatBoxes.layout;
+        const listingLocation = docRef.data().flatBoxes.location;
+        const listingName = `${listingLayout}${listingLayout ? " " : ""}${listingLocation}`;
+        const listingPersonBio = docRef.data().personBio;
+        const listingFlatBio = docRef.data().flatBio;
+        const listingPersonBoxes = JSON.stringify(docRef.data().personBoxes);
+        const listingPersonTags = JSON.stringify(docRef.data().personTags);
+        const listingFlatBoxes = JSON.stringify(docRef.data().flatBoxes);
+        const statusCode = 200;
+        return {
+            props: {
+                listingName,
+                listingPersonBio,
+                listingFlatBio,
+                listingPersonBoxes,
+                listingPersonTags,
+                listingFlatBoxes,
+                statusCode,
+            },
+        }
+    }catch(error){
+        const listingName = null;
+        const listingPersonBio = null;
+        const listingFlatBio = null;
+        const listingPersonBoxes = null;
+        const listingPersonTags = null;
+        const listingFlatBoxes = null;
+        const statusCode = 200;
+        return {
+            props: {
+                listingName,
+                listingPersonBio,
+                listingFlatBio,
+                listingPersonBoxes,
+                listingPersonTags,
+                listingFlatBoxes,
+                statusCode,
+            },
+        }
+    }
+}
 
 
--- pages/flatmate/[id].jsx
+++ pages/flatmate/[id].jsx
@@ -1,11 +1,62 @@
 import React from 'react'
 import Listing from '../../components/Listing/Listing'
+//Firebase
+import { db } from '../../Firebase';
+import { getDoc, doc } from 'firebase/firestore';
+import { auth } from '../../Firebase';
 
-const FlatListing = () => {
+const FlatListing = (props) => {
     return (
-        <Listing type="flatmate"/>
+        <Listing type="flatmate"
+        listingName={props.listingName}
+        listingBio={props.listingBio}
+        listingPersonBoxes={JSON.parse(props.listingPersonBoxes)}
+        listingFlatTags={JSON.parse(props.listingFlatTags)}
+        listingPersonTags={JSON.parse(props.listingPersonTags)}
+        statusCode={props.statusCode}
+        />
     )
 }
 
 export default FlatListing
+
+export async function getServerSideProps(context) {
+    const {id} = context.params;
+    try{    
+        const docRef = await getDoc(doc(db, "listings", id));
+        const listingName = docRef.data().userInfo.username;
+        const listingBio = docRef.data().bio;
+        const listingPersonBoxes = JSON.stringify(docRef.data().personBoxes);
+        const listingFlatTags =  JSON.stringify(docRef.data().flatTags);
+        const listingPersonTags = JSON.stringify(docRef.data().personTags);
+        const statusCode = 200;
+        return {
+            props: {
+                listingName,
+                listingBio,
+                listingPersonBoxes,
+                listingFlatTags,
+                listingPersonTags,
+                statusCode,
+            },
+        }
+    }catch(error){
+        const listingName = null;
+        const listingBio = null;
+        const listingPersonBoxes = null;
+        const listingFlatTags = null;
+        const listingPersonTags = null;
+        const statusCode = error.message
+        return {
+            props: {
+                listingName,
+                listingBio,
+                listingPersonBoxes,
+                listingFlatTags,
+                listingPersonTags,
+                statusCode,
+            },
+        }
+    }
+}
 
