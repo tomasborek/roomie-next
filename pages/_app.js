e4cd4395db6ea82fa2f7cb62c976c93f33056b23
tomasborek <tomasborek3@gmail.com>
2022-01-03 12:27:53 +0100

    Byl přidán Nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,21 @@
 import Layout from '../components/Layout/Layout'
 //next
 import Head from 'next/dist/shared/lib/head';
+//Nprogress
+import NProgress from 'nprogress';
+import "nprogress/nprogress.css";
+import Router from 'next/router';
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 //Componentns
 
 //Context
@@ -15,12 +30,14 @@ import { ExploreProvider } from '../contexts/ExploreContext';
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
-  
+
   return (
     <>
     <Head>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossOrigin="anonymous" referrerpolicy="no-referrer" />
       <meta name="description" content="Portál Roomie se zaměřuje na zprostředkování moderní platformy pro hledání spolubydlících či bytů ke spolubydlení. Hledejte spolubydlení a spolubydlící jednoduše a podle vašich představ. Jsme sociální sítí pro spolubydlení."></meta>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9969104196645961"
+     crossOrigin="anonymous"></script>
     </Head>
     <LoadingProvider>
         <SnackBarProvider>
