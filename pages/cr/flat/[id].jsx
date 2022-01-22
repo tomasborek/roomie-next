1851bc140b091477e2a585cd9af4fd25ff819d6b
tomasborek <tomasborek3@gmail.com>
2022-01-22 11:48:05 +0100

    Cr listing byl vyměněn za listing, byl oblkopen providerem a naplněn nullovými ssr props (client-side)

--- pages/cr/flat/[id].jsx
+++ pages/cr/flat/[id].jsx
@@ -2,15 +2,43 @@ import React,{useEffect, useRef, useState} from 'react'
 //next
 import Head from "next/head"
 // Components
-import CrListing from "../../../components/Listing/CrListing";
+import Listing from "../../../components/Listing/Listing";
+//Contexts
+import ListingProvider from "../../../contexts/ListingContext";
 
 const CreateFlatListing = () => {
+    const listingName = null;
+    const listingUsername = null;
+    const listingAge = null;
+    const listingGender = null;
+    const listingBio = null;
+    const listingPersonBoxes = null;
+    const listingFlatTags = null;
+    const listingPersonTags = null;
+    const listingPremium = null;
+    const listingFans = null;
+    const status = "client-side";
+    const ssrProps = {
+            listingName,
+            listingUsername,
+            listingAge,
+            listingGender,
+            listingBio,
+            listingPersonBoxes,
+            listingFlatTags,
+            listingPersonTags,
+            listingPremium,
+            listingFans,
+            status,
+        }
     return (
         <>
          <Head>
             <title>Vytvořte svůj inzerát | Roomie</title>
         </Head>
-        <CrListing type="flat"/>
+        <ListingProvider type="flat" cr={true} ssrProps={ssrProps}>
+           <Listing/>
+        </ListingProvider>
     </>
     )
 }
--- pages/cr/flatmate/[id].jsx
+++ pages/cr/flatmate/[id].jsx
@@ -1,15 +1,43 @@
 import React, {useEffect, useState, useRef} from 'react'
+import Listing from '../../../components/Listing/Listing';
 //next
 import Head from "next/head"
-import CrListing from "../../../components/Listing/CrListing";
+//Contexts
+import {ListingProvider} from "../../../contexts/ListingContext";
 
 const CreateFlatmateListing = () => {
+        const listingName = null;
+        const listingUsername = null;
+        const listingAge = null;
+        const listingGender = null;
+        const listingBio = null;
+        const listingPersonBoxes = null;
+        const listingFlatTags = null;
+        const listingPersonTags = null;
+        const listingPremium = null;
+        const listingFans = null;
+        const status = "client-side";
+        const ssrProps = {
+                listingName,
+                listingUsername,
+                listingAge,
+                listingGender,
+                listingBio,
+                listingPersonBoxes,
+                listingFlatTags,
+                listingPersonTags,
+                listingPremium,
+                listingFans,
+                status,
+            }
     return (
         <>
         <Head>
             <title>Vytvořte svůj inzerát | Roomie</title>
         </Head>
-        <CrListing type="flatmate"/>
+        <ListingProvider type="flatmate" cr={true} ssrProps={ssrProps}>
+            <Listing/>
+        </ListingProvider>
     </>
     )
 }
