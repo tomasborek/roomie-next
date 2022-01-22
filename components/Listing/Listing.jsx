f28c04389a94b3524f162d6f848a42a514698a50
tomasborek <tomasborek3@gmail.com>
2022-01-22 12:02:02 +0100

    Opening boxes byly vloženy do vlastního komponentu

--- components/Listing/Listing.jsx
+++ components/Listing/Listing.jsx
@@ -16,6 +16,7 @@ import ListingDialogs from './ListingDialogs/ListingDialogs';
 //Mui components
 import {Skeleton, Collapse, Alert} from '@mui/material';
 import ListingHeader from './ListingHeader/ListingHeader';
+import ListingOpeningBoxes from './ListingOpeningBoxes/ListingOpeningBoxes';
 
 
 
@@ -31,11 +32,14 @@ const Listing = () => {
         listingId,
         listingName,
         listingPersonBoxes,
+        addedPersonBoxes,
+        listingFlatBoxes,
+        addedFlatBoxes,
         listingPremium,
         setPersonBoxerOverlay,
-        addedPersonBoxes,
         handleSave,
     } = useListing();
+
     const [flatBoxesInfoAlert, setFlatBoxesInfoAlert] = useState(true);
     const [personBoxesInfoAlert, setPersonBoxesInfoAlert] = useState(true);
     if(type === "flatmate"){
@@ -184,55 +188,8 @@ const Listing = () => {
                                                     <h3>Premium uživatel</h3>
                                                 </div>
                                             }
-                                            <div className="body-opening-boxes">
-                                                <div className={`boxes-profile-info ${listingPremium && "premium"}`}>
-                                                    <div className="profile-info-pfp-container">
-                                                        {editListing && 
-                                                            <div onClick={() => setGalleryInput({
-                                                                open: true,
-                                                                index: -1,
-                                                            })} className="pfp-container-edit-icon">
-                                                                <i className="fas fa-camera"></i>
-                                                            </div>
-                                                        }
-                                                        {addedPfp ?
-                                                            <img src={URL.createObjectURL(addedPfp)} className='profile-info-pfp' />
-                                                            :
-                                                            <>
-                                                                {(listingInfo && pfp) ? 
-                                                                    <img src={pfp} className='profile-info-pfp' /> 
-                                                                    : 
-                                                                    listingInfo ? 
-                                                                    <img src={`/img/pfps/${(listingGender === "male") ? "radek" : "radka"}-pfp.png`} className="profile-info-pfp" /> 
-                                                                    : 
-                                                                    <div className="profile-info-pfp"></div>
-                                                                }
-                                                            </>
-                                                        }
-                                                    </div>
-                                                    <div className="profile-info-text">
-                                                        {(!listingInfo && !listingUsername) ? 
-                                                            <Skeleton variant="text" sx={{width: 50}}/>
-                                                            : 
-                                                            <p className="text-name">{listingUsername}</p> 
-                                                        } 
-                                                        {(!listingInfo && !listingUsername) ? 
-                                                            <Skeleton variant="text" sx={{width: 30}} />
-                                                            :  
-                                                            <p className="text-description">
-                                                                {listingGender === "male" ? "Muž" : listingGender === "female" ? "Žena" : listingGender === "other" ? "Jiné" : ""}, {listingAge}
-                                                            </p>
-                                                        }
-                                                    </div>
-                                                </div>
-
-                                                <div className="boxes-map-container" >
-                                                    <div className="map-overlay">
-                                                        <p>Již brzy <i className="fas fa-lock"></i></p>
-                                                    </div>
-                                                    <img src="/img/listing/mapa.png" alt="" className="boxes-map" />
-                                                </div>
-                                            </div>
+                                           
+                                           <ListingOpeningBoxes/>
 
                                             <ListingBoxesContainer type="flat" addedBoxes={addedFlatBoxes} existingBoxes={listingFlatBoxes} editListing={editListing}/> 
 
