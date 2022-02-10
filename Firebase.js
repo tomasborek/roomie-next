076537fd822bced35207ddd24b163e23b22865f6
tomasborek <tomasborek3@gmail.com>
2022-02-10 17:34:19 +0100

    Byl přidán app check

--- Firebase.js
+++ Firebase.js
@@ -4,6 +4,7 @@ import {getFirestore} from "firebase/firestore";
 import {getFunctions} from "firebase/functions";
 import {getStorage} from "firebase/storage";
 import {getAnalytics} from "firebase/analytics";
+import {initializeAppCheck, ReCaptchaV3Provider} from "firebase/app-check"
 
 //Dev
 // const firebaseConfig = {
@@ -25,8 +26,19 @@ const firebaseConfig = {
   appId: "1:901977154768:web:deafa61833a6055f27ead5",
   measurementId: "G-S8Q9W3DW20"
 };
+
+const app = initializeApp(firebaseConfig);
+
+
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LcEZG4eAAAAAGVZpuut_SvxobsCEos9tjjJMePD'),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
+
 // Initialize Firebase
-initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const db = getFirestore();
 export const functions = getFunctions();
