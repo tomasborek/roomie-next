83c37fa8e21c6e136216ae3bc8bb5fc7c840c1ae
tomasborek <tomasborek3@gmail.com>
2022-02-12 10:10:16 +0100

    Byla přidána language kvůli SEO a také bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   trailingSlash: true,
-}
+  i18n: {
+    locales: ["cs"],
+    defaultLocale: "cs",
+  },
+});
