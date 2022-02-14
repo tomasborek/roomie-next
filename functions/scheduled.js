e8722b1fff34a40cb1bb3a0a0de4d0821c01d48b
tomasborek <tomasborek3@gmail.com>
2022-02-14 20:34:06 +0100

    Funkce byla změněna na maniuální spouštění

--- functions/scheduled.js
+++ functions/scheduled.js
@@ -2,50 +2,32 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const db = admin.firestore();
 
-exports.birthday = functions.pubsub.schedule("0 1 * * *").timeZone("Europe/Prague").onRun((context) => {
-        const now = admin.firestore.Timestamp.now();
-        return db.collection("users").where("mainInfo.birthday", "<=", now).get().then((users) => {
-            console.log("Found " + users.docs.length + " users with outdated age.");
-            return Promise.all(
-                users.docs.map((user) => {
-                    const currentAge = user.data().mainInfo.age;
-                    const newAge = currentAge + 1;
-                    const listingId = user.data().listing.id;
-                    return db.collection("users").doc(user.id).update({
-                        "mainInfo.age": newAge,
-                    }).then((response) => {
-                        return db.collection("listings").doc(listingId).update({
-                            "userInfo.age": newAge,
-                        })
-                    })
-                })
-            )
-        })
-})
-
 exports.birthdayCallable = functions.https.onRequest((req, res) => {
-    const now = admin.firestore.Timestamp.now();
-    return db.collection("users").where("mainInfo.birthday", "<=", now).get().then((users) => {
-        console.log("Found " + users.docs.length + " with outdated birthday.");
+    let now = admin.firestore.Timestamp.now();
+    now = now.toDate();
+    return db.collection("users").get().then((allUsers) => {
         return Promise.all(
-            users.docs.map((user) => {
-                const currentAge = user.data().mainInfo.age;
-                const newAge = currentAge + 1;
-                const listingId = user.data().listing.id;
+            allUsers.docs.map((user) => {
+                let birthday = user.data().mainInfo.birthday;
+                birthday = birthday.toDate();
+                const diff = now - birthday;
+                const ageDate = new Date(diff);
+                const age = Math.abs(ageDate.getFullYear() - 1970);
                 return db.collection("users").doc(user.id).update({
-                    "mainInfo.age": newAge,
+                    "mainInfo.age": age,
                 }).then((response) => {
-                    return db.collection("listings").doc(listingId).update({
-                        "userInfo.age": newAge,
+                    db.collection("listings").doc(user.data().listing.id).update({
+                        "userInfo.age": age,
                     })
-                })
+                }) 
             })
         )
     }).then((response) => {
         res.status(200).send("Success");
     }).catch((error) => {
-        res.status(500).send(error);
+        res.status(500).send(error.message);
     })
 })
+
 
 
