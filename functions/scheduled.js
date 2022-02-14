7f1a13a2489c093f37cdca07bb90755cb087c078
tomasborek <tomasborek3@gmail.com>
2022-02-14 13:59:55 +0100

    Bug fix: Birthday funkce + byla přidána callable verze na request

--- functions/scheduled.js
+++ functions/scheduled.js
@@ -2,30 +2,50 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const db = admin.firestore();
 
-exports.birthday = functions.pubsub.schedule("0 1 * * *")
-    .timeZone("Europe/Prague")
-    .onRun((context) => {
+exports.birthday = functions.pubsub.schedule("0 1 * * *").timeZone("Europe/Prague").onRun((context) => {
         const now = admin.firestore.Timestamp.now();
-        return db.collection("users").where("birthday", "<=", now).get().then((users) => {
+        return db.collection("users").where("mainInfo.birthday", "<=", now).get().then((users) => {
+            console.log("Found " + users.docs.length + " users with outdated age.");
             return Promise.all(
                 users.docs.map((user) => {
                     const currentAge = user.data().mainInfo.age;
-                    const age = currentAge + 1;
+                    const newAge = currentAge + 1;
                     const listingId = user.data().listing.id;
-                    return new Promise((resolve, reject) => {
-                        db.collection("users").doc(user.id).update({
-                            "mainInfo.age": age,
-                        }).then((response) => {
-                            return db.collection("listings").doc(listingId).update({
-                                "userInfo.age": age,
-                            })
-                        }).then((response) => {
-                            resolve(response);
-                        }).catch((error) => {
-                            reject(error);
+                    return db.collection("users").doc(user.id).update({
+                        "mainInfo.age": newAge,
+                    }).then((response) => {
+                        return db.collection("listings").doc(listingId).update({
+                            "userInfo.age": newAge,
                         })
                     })
                 })
             )
         })
+})
+
+exports.birthdayCallable = functions.https.onRequest((req, res) => {
+    const now = admin.firestore.Timestamp.now();
+    return db.collection("users").where("mainInfo.birthday", "<=", now).get().then((users) => {
+        console.log("Found " + users.docs.length + " with outdated birthday.");
+        return Promise.all(
+            users.docs.map((user) => {
+                const currentAge = user.data().mainInfo.age;
+                const newAge = currentAge + 1;
+                const listingId = user.data().listing.id;
+                return db.collection("users").doc(user.id).update({
+                    "mainInfo.age": newAge,
+                }).then((response) => {
+                    return db.collection("listings").doc(listingId).update({
+                        "userInfo.age": newAge,
+                    })
+                })
+            })
+        )
+    }).then((response) => {
+        res.status(200).send("Success");
+    }).catch((error) => {
+        res.status(500).send(error);
     })
+})
+
+
