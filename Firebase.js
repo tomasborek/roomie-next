3e84dcdab75b9d839879326432ff1f6198eee79a
tomasborek <tomasborek3@gmail.com>
2022-02-14 11:07:52 +0100

    Bug fix

--- Firebase.js
+++ Firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 import {getFunctions} from "firebase/functions";
@@ -29,7 +29,8 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+
+const app = initializeApp(firebaseConfig); 
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
