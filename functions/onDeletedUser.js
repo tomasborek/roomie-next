8233ce9c8bd9721c79cf844d346894d365f9821d
tomasborek <tomasborek3@gmail.com>
2022-02-14 09:03:26 +0100

    Při smazání usera se smažou i requesty

--- functions/onDeletedUser.js
+++ functions/onDeletedUser.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const { db } = require("../Firebase");
 // Every function that happens on deleted user, either called or triggered by auth
 
 //Triggered
@@ -14,9 +15,15 @@ exports.deleteUser = functions.auth.user().onDelete((user) => {
 exports.deleteListing = functions.auth.user().onDelete((user) => {
     const collectionRef = admin.firestore().collection("listings");
     const query = collectionRef.where("userInfo.uid", "==", user.uid);
+    let listingId;
     return query.get().then((listings) => {
-      return collectionRef.doc(listings.docs[0].id).delete();
-    });
+        listingId = listings.docs[0].id;
+      return collectionRef.doc(listingId).delete();
+    }).then((response) => {
+        return collectionRef.doc(listingId).collection("contact").get();
+    }).then((docs) => {
+        return collectionRef.doc(listingId).collection("contact").doc(docs.docs[0].id).delete();
+    })
 });
 
 exports.deleteFriends = functions.firestore.document("users/{userId}/friends/{friendId}").onDelete((snap, context) => {
@@ -26,6 +33,24 @@ exports.deleteFriends = functions.firestore.document("users/{userId}/friends/{fr
     const uid = snap.ref.parent.parent.id;
     return usersCollection.doc(friendId).collection("friends").doc(uid).delete();
 
+})
+
+exports.deleteRequests = functions.auth.user().onDelete((user) => {
+    return db.collection("users").doc(user.uid).collection("sentRequests").get().then((sentRequests) => {
+        return Promise.all(
+            sentRequests.docs.map((sentRequest) => {
+                return db.collection("users").doc(user.uid).collection("sentRequests").doc(sentRequest.id).delete();
+            })
+        )
+    }).then((response) => {
+        return db.collection("users").doc(user.uid).collection("recievedRequests").get();
+    }).then((recievedRequests) => {
+        return Promise.all(
+            recievedRequests.docs.map((recievedRequest) => {
+                return db.collection("users").doc(user.uid).collection("recievedRequests").doc(recievedRequest.id).delete();
+            })
+        )
+    })
 })
 
 // Delete storage record of a user
