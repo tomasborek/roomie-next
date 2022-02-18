f158ce258b9455731c9303c139e4bd02ea344f2d
tomasborek <tomasborek3@gmail.com>
2022-02-18 21:58:49 +0100

    Formatování

--- Firebase.js
+++ Firebase.js
@@ -1,9 +1,9 @@
-import {initializeApp} from "firebase/app";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
-import {getFunctions} from "firebase/functions";
-import {getStorage} from "firebase/storage";
-import {getAnalytics} from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getFunctions } from "firebase/functions";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 //Dev
 // const firebaseConfig = {
@@ -23,14 +23,12 @@ const firebaseConfig = {
   storageBucket: "roomie-prod-24d36.appspot.com",
   messagingSenderId: "901977154768",
   appId: "1:901977154768:web:deafa61833a6055f27ead5",
-  measurementId: "G-S8Q9W3DW20"
+  measurementId: "G-S8Q9W3DW20",
 };
-
-
 
 // Initialize Firebase
 
-const app = initializeApp(firebaseConfig); 
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
