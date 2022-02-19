f86846658878b13a7b14a290530dcb2327e74a50
tomasborek <tomasborek3@gmail.com>
2022-02-19 09:39:11 +0100

    Formátování

--- pages/requests/recieved.jsx
+++ pages/requests/recieved.jsx
@@ -1,31 +1,45 @@
-import React  from 'react';
+import React from "react";
 //Layout
-import PageFeedLayout from '../../components/PageFeedLayout/PageFeedLayout';
+import PageFeedLayout from "../../components/PageFeedLayout/PageFeedLayout";
 //next
 import Head from "next/head";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 //COmponents
-import ReqFeed from '../../components/ReqFeed/ReqFeed';
+import ReqFeed from "../../components/ReqFeed/ReqFeed";
 // Context
 
 const RecievedRequests = () => {
-    const router = useRouter();
-    return (
-        <>
-            <Head>
-                <title>Příchozí žádosti | Roomie</title>
-            </Head>
-            <PageFeedLayout heading={"Žádosti"}>
-                    <ul className="content-nav">
-                        <li onClick={() => router.push("/requests/recieved")} className={`nav-link ${router.pathname === "/requests/recieved" && "active"}`}>Příchozí</li>
-                        <li onClick={() => router.push("/requests/sent")} className={`nav-link ${router.pathname === "/requests/sent" && "active"}`}>Odeslané</li>
-                    </ul>
-                    <div className="content-feed">
-                        <ReqFeed type="recieved"/>
-                    </div>
-            </PageFeedLayout>
-        </>
-    )
-}
+  const router = useRouter();
+  return (
+    <>
+      <Head>
+        <title>Příchozí žádosti | Roomie</title>
+      </Head>
+      <PageFeedLayout heading={"Žádosti"}>
+        <ul className="content-nav">
+          <li
+            onClick={() => router.push("/requests/recieved")}
+            className={`nav-link ${
+              router.pathname === "/requests/recieved" && "active"
+            }`}
+          >
+            Příchozí
+          </li>
+          <li
+            onClick={() => router.push("/requests/sent")}
+            className={`nav-link ${
+              router.pathname === "/requests/sent" && "active"
+            }`}
+          >
+            Odeslané
+          </li>
+        </ul>
+        <div className="content-feed">
+          <ReqFeed type="recieved" />
+        </div>
+      </PageFeedLayout>
+    </>
+  );
+};
 
-export default RecievedRequests
+export default RecievedRequests;
